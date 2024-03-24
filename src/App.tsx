404ab620cd118a272af0b81b2d6a3affe27db459
augustodomingos28 <augusto.domingos@unimedsc.coop.br>
2024-03-24 03:45:01 -0300

    iniciando a criação da parte visual - aula 2

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { RouterProvider } from "react-router-dom";
+import "./global.css";
+import { router } from "./router";
+import { Helmet, HelmetProvider } from "react-helmet-async";
+import { Toaster } from "sonner";
+
+export function App() {
+  return (
+    <HelmetProvider>
+      <Toaster richColors />
+      <Helmet titleTemplate="%s | Pizza Shop" />
+      <RouterProvider router={router} />
+    </HelmetProvider>
+  );
+}
